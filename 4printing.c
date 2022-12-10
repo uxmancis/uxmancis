dedc716e62038e3ff8436b446be42722bbaff621
Uxue <116915535+uxmancis@users.noreply.github.com>
2022-12-10 14:27:35 +0100

    update

--- 4printing.c
+++ 4printing.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   4printing.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: uxmancis <uxmancis@student.42urduliz.co    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/06 17:48:18 by uxmancis          #+#    #+#             */
-/*   Updated: 2022/12/06 18:05:57 by uxmancis         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "ft_printf.h"
 
 int	printchar(char c, int num_printed)
@@ -25,7 +13,6 @@ int	putstr(char *str, int num_printed)
 	char	c;
 
 	i = 0;
-
 	while (str[i] != '\0')
 	{
 		c = str[i];
@@ -35,15 +22,22 @@ int	putstr(char *str, int num_printed)
 	return (num_printed);
 }
 
-//write only works with chars, not with ints. As a consequence, we have to chop numbers into becoming <10 so that we can write them as chars by ASCII table conversion (+48)
+//Write only works with chars, not with ints. 
+//As a consequence, we've to chop numbers into making them <10 so that...
+//...we can write them as chars by ASCII table conversion (+48)
 int	putnbr(int arg, int num_printed)
 {
+	if(arg == -2147483648)
+	{
+		num_printed = printchar('-', num_printed);
+		num_printed = putstr("2147483648", num_printed);
+		return(num_printed);
+	}
 	if (arg < 0)
 	{
 		num_printed = printchar('-', num_printed);
 		arg = arg * (-1);
 	}
-
 	if (arg >= 10)
 	{
 		num_printed = putnbr((arg / 10), num_printed);
